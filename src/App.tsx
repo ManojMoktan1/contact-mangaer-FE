584dcc9947987487be96aa381362f76f7fa8246e
ManojMoktan1 <moktanmanoj1@gmail.com>
2022-08-16 15:28:01 +0545

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Contact from './components/Contact/Contact';
+import axios, { AxiosRequestConfig } from 'axios';
+import { createInterceptors } from './utils/interceptor';
+import ListContacts from './pages/ListContacts';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
+  createInterceptors();
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/contacts' element={<Contact />} />
+        <Route path='/contacts' element={<NavBar />} >
+          <Route index element={<ListContacts />} />
+          <Route path='/contacts/add' element={<Contact />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
